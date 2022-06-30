cc326d7d4b22deb2e5f9d759d6a805c53c38ddd3
borisavila539 <borisavila539@gmail.com>
2022-06-29 20:21:16 -0600

    gastos de viaje 5

--- Gira-App/src/Screens/Viaje.js
+++ Gira-App/src/Screens/Viaje.js
@@ -3,15 +3,28 @@ import { Button, StyleSheet, View, ScrollView, SafeAreaView  } from "react-nativ
 import { TextInputContainer, SendButton } from "../Components/indexComponents";
 import DatePicker from 'react-native-date-picker'
 const Viajes = (props) =>{
+    const [nFactura,SetNFactura] = useState('')
     const [subTotal, setSubTotal] = useState(0.00)
     const [isv,setIsv] = useState(0.00)
     const [ist, setIst] = useState(0.00)
     const [total, setTotal] = useState(0.00)
+
+    const onChanceNFactura = (value) =>{
+
+        if(value.length==16){
+            SetNFactura(value[0]+value[1]+value[2]+'-'+value[3]+value[4]+value[5]+'-'+value[6]+value[7]+'-'+value[8]+value[9]+value[10]+value[11]+value[12]+value[13]+value[14]+value[15])
+        }else if(value.length==17){ 
+            value = value[0]+value[1]+value[2]+value[4]+value[5]+value[6]+value[8]+value[9]+value[11]+value[12]+value[13]+value[14]+value[15]+value[16]
+            SetNFactura(value);
+        }else{
+            SetNFactura(value);
+        }
+    }
     return(
         <ScrollView contentContainerStyle={styles.scrollview}>
             <SafeAreaView  style={styles.container}>
-                <TextInputContainer title='RTN: ' placeholder='XXXXXXXXXXXXXX'/>
-                <TextInputContainer title='N. Factura: ' placeholder='XXX-XXX-XX-XXXXXXXX'/>
+                <TextInputContainer title='RTN: ' placeholder='XXXXXXXXXXXXXX'  maxLength={14}/>
+                <TextInputContainer title='N. Factura: ' placeholder='XXX-XXX-XX-XXXXXXXX' value={nFactura} onChangeText={(value)=>onChanceNFactura(value)} maxLength={19}  teclado='decimal-pad'/>
                 <TextInputContainer title='Descripcion: ' multiline={true} maxLength={300}/>
                 <TextInputContainer title='SubTotal: ' placeholder='999.99' teclado='decimal-pad'
                 onChangeText={(value)=>{
